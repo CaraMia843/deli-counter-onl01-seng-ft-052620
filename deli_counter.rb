b7f6163c7377bdaff45efe86c2079b2109e42c5f
Cara Zellner <carajeanine@gmail.com>
2020-06-03 12:12:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def line(array)
+  array.map!.with_index(1) do |name, index|
+    "#{index}. #{name}"
+  end
+
+  if array.length > 0
+    puts "The line is currently: #{array.join(" ")}"
+  else
+    puts "The line is currently empty"
+  end
+end
